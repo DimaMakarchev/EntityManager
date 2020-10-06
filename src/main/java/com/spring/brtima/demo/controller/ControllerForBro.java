cdd57c2788b333188296534e1ca19297e079754b
Dima Makarchev <d.makarchev2411@gmail.com>
2020-10-06 17:59:43 +0300

    new x

--- src/main/java/com/spring/brtima/demo/controller/ControllerForBro.java
+++ src/main/java/com/spring/brtima/demo/controller/ControllerForBro.java
@@ -20,7 +20,7 @@ public class ControllerForBro extends Name implements NameInterface{
 
 
     @RequestMapping("/")
-    public List<Object> methodGetEntityBro() {
+    public List<Object> methodGetEntityBro()  {
             NAMME
         CriteriaBuilder criteriaBuilder =  entityManager
                 .getCriteriaBuilder();
